7d65dddda50a5aa4bf2d570380a3a55005204cc8
LASGLOWTCH <omozemojeau@gmail.com>
2023-03-18 05:40:54 +0100

    further commit

--- src/app.js
+++ src/app.js
@@ -29,55 +29,55 @@ app.get("/", (req, res)=>{
 
 // use app.post to post the data in your website
 app.post("/", (req, res)=>{
-//  var Name= req.body.Name;
-//  var email= req.body.email;
-//  var subject=  req.body.subject;
-//  var message=  req.body.message;
+ var Name= req.body.Name;
+ var email= req.body.email;
+ var subject=  req.body.subject;
+ var message=  req.body.message;
  
-//  console.log(Name);
+ console.log(Name);
 
 
 //  using mailchimp to arrange your data
-//  const data = {
-//     members:[{email_address:email, 
-//      status:"subscribed",
-//     merge_fields:{ 
-//      NAME:Name, 
-//      SUBJECT:subject,
-//      MESSAGE:message,} }]
-// };
+ const data = {
+    members:[{email_address:email, 
+     status:"subscribed",
+    merge_fields:{ 
+     NAME:Name, 
+     SUBJECT:subject,
+     TEXTAREA:message,} }]
+};
 
 //  convert your javascript object above to a JSON FORMAT
-//  const jsonData=JSON.stringify(data);
-//  console.log(jsonData);
+ const jsonData=JSON.stringify(data);
+ console.log(jsonData);
 
-//  const key=process.env.API_RANDOM_TOKEN;
- 
-// const url="https://us11.api.mailchimp.com/3.0/lists/9ff7bb9af5";
+ const key=process.env.API_RANDOM_TOKEN;
+const id= process.env.LIST_ID;
+const url="https://us11.api.mailchimp.com/3.0/lists/333afc14d1";
 
 
-// const options= {
-//            method: "POST", 
-//            auth:"austinosaz: key" }
-//            console.log(key)
+const options= {
+           method: "POST", 
+           auth:"austinosaz: dfb9dfbc45265613af9f692d274281ed-us11" }
+           
          
 
-//  const request= https.request( url, options, (response)=>{
-//     // if page is successful show success website
-//     console.log(response)
-//     if(response.statusCode === 200){
-//         res.sendFile( __dirname + "/success.html")
-//     }
-//     // else show failure website
-//     else{res.sendFile(__dirname+ "/failure.html")}
-//     response.on("data", (data)=> {
-//         console.log(JSON.parse(data));
+ const request= https.request( url, options, (response)=>{
+    // if page is successful show success website
+    console.log(response)
+    if(response.statusCode === 200){
+        res.sendFile( __dirname + "/success.html")
+    }
+    // else show failure website
+    else{res.sendFile(__dirname+ "/failure.html")}
+    response.on("data", (data)=> {
+        console.log(JSON.parse(data));
 
-    // });
+    });
 // When the failure wbsite   loads a button would redirect you to te signup page
-//     app.post("/failure", (req, res)=>{
-//         res.redirect("/")})
-//  });
+    app.post("/failure", (req, res)=>{
+        res.redirect("/")})
+ });
 request.write(jsonData);
 request.end();
 

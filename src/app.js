7ba0e3f967970312ef0864e2517b0cacfbafcc15
LASGLOWTCH <omozemojeau@gmail.com>
2023-03-18 22:25:02 +0100

    commit

--- src/app.js
+++ src/app.js
@@ -1,9 +1,5 @@
 
-
 // require express, body-parser, request, https
-
-
-
 
 const express= require ("express");
 const bodyparser= require("body-parser");
@@ -12,6 +8,7 @@ const https= require("https");
 
 
  const app= express();
+ const mailchimp = require("@mailchimp/mailchimp_marketing");
  require('dotenv').config();
 
 // use to display static css page styling
@@ -27,6 +24,15 @@ app.get("/", (req, res)=>{
 })
 
 
+
+
+ app.listen(process.env.PORT ||3000, ()=>{
+    console.log("server is running on port 3000 ")
+    
+ });
+
+
+
 // use app.post to post the data in your website
 app.post("/", (req, res)=>{
  var Name= req.body.Name;
@@ -37,62 +43,75 @@ app.post("/", (req, res)=>{
  console.log(Name);
 
 
-//  using mailchimp to arrange your data
- const data = {
-    members:[{email_address:email, 
-     status:"subscribed",
-    merge_fields:{ 
-     NAME:Name, 
-     SUBJECT:subject,
-     TEXTAREA:message,} }]
+//  const key=process.env.API_RANDOM_TOKEN;
+//  const ID= process.env.LIST_ID;
+
+console.log(process.env.API_RANDOM_TOKEN);
+// mail chimp auth
+ mailchimp.setConfig({
+  apiKey: "dfb9dfbc45265613af9f692d274281ed;",
+  server: "us11",
+});
+
+
+// //  convert your javascript object above to a JSON FORMAT
+
+
+const run = async () => {  
+  const response = await mailchimp.lists.batchListMembers(" 333afc14d1", {
+      members: [
+        {email_address:email, 
+          status:"subscribed",
+         merge_fields:{ 
+          NAME:Name, 
+          SUBJECT:subject,
+          TEXTAREA:message,}
+  }],
+
+  });
+
+  if (response.error_count) {
+      res.sendFile(__dirname + "/failure.html");
+  } else {
+      res.sendFile(__dirname + "/success.html");
+  }
 };
 
-//  convert your javascript object above to a JSON FORMAT
- const jsonData=JSON.stringify(data);
- console.log(jsonData);
+run();
+   
 
- const key=process.env.API_RANDOM_TOKEN;
-const id= process.env.LIST_ID;
-const url="https://us11.api.mailchimp.com/3.0/lists/333afc14d1";
+// //  using mailchimp to arrange your data
+ 
 
 
-const options= {
-           method: "POST", 
-           auth:"austinosaz: dfb9dfbc45265613af9f692d274281ed-us11" }
-           
-         
+//  const request= https.request( url, options, (response)=>{
+//     // if page is successful show success website
+//     console.log(response)
+//     if(response.statusCode === 200){
+//         res.sendFile( __dirname + "/success.html")
+//     }
+//     // else show failure website
+//     else{res.sendFile(__dirname+ "/failure.html")}
+//     response.on("data", (data)=> {
+//         console.log(JSON.parse(data));
 
- const request= https.request( url, options, (response)=>{
-    // if page is successful show success website
-    console.log(response)
-    if(response.statusCode === 200){
-        res.sendFile( __dirname + "/success.html")
-    }
-    // else show failure website
-    else{res.sendFile(__dirname+ "/failure.html")}
-    response.on("data", (data)=> {
-        console.log(JSON.parse(data));
+//     });
+// // When the failure wbsite   loads a button would redirect you to te signup page
+//     app.post("/failure", (req, res)=>{
+//         res.redirect("/")})
+//  });
 
-    });
-// When the failure wbsite   loads a button would redirect you to te signup page
-    app.post("/failure", (req, res)=>{
-        res.redirect("/")})
- });
-request.write(jsonData);
-request.end();
+// request.write(jsonData);
+// request.end();
 
 
 
 
 })
 
+app.post("/failure", (req, res) => {
+    res.redirect("/");
+});
 
 
 
-
-
-
- app.listen(process.env.PORT ||3000, ()=>{
-    console.log("server is running on port 3000 ")
-    
- });
